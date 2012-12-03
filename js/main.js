fd8318d22324f731b4c31180382908f773534cdf
Latronix Omnigenus <latronix@lra-null-linux.com>
2012-12-03 01:09:31 -0400

    OmniPHP: Initial Commit (ignore)
    ignore: nothing useful yet, just for backup purposes

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    //alert("hello world...");
+    
+    $("#OmniPHP_Form").validate({
+        rules: {
+                CellPhone: {
+                        required: true,
+                        phoneUS: true,
+                        minlength: 12,
+                        maxlength: 12
+                }
+        },
+        messages: {
+                CellPhone: {
+                        required: "Please enter a phone",
+                        phoneUS: "Enter phone in correct format",
+                        minlength: "Your phone must consist of at least 12 characters",
+                        maxlength: "Your phone cannot exceed 12 characters"
+                }
+        }
+    });
+    
+});
